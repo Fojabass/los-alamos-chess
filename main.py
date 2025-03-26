0c4bdadc2773afca1331530614fe928f1e8c4cc6
Julien Devol <julien.devol@gmail.com>
2025-03-26 11:02:56 -0400

    Tweaked parts of the main loop

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 running = True
+delta = 0
+
+pygame.display.set_caption("Los Alamos Chess")
 
 while running:
     for event in pygame.event.get():
-        pass
+        if event.type == pygame.QUIT:
+            running = False
 
     screen.fill("purple")
     pygame.display.flip()
+    delta = clock.tick(60) / 1000
+
+pygame.quit()
 
 
     
