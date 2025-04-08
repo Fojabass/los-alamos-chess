ad5e1d4ca3774be97467f8dee27e76806bd44094
Julien Devol <julien.devol@gmail.com>
2025-04-08 14:07:10 -0400

    Implemented piece rendering on the chess board

--- src/board.py
+++ src/board.py
@@ -4,72 +4,122 @@
 import pygame
 from typing import List, Tuple, Optional
 
-# The logic so far moves from Board -> Square -> Piece.
-# Board handles the high-level logic, Square handles individual cell behavior, and Piece defines how 
+DIMENSIONS: int = 6
 
 class Board:
-    DIMENSIONS: int = 6
     SCALE_MODIFIER: float = 0.80
     WHITE: Tuple[int, int, int] = (240, 217, 181)
     BLACK: Tuple[int, int, int] = (181, 136, 99)
 
-    # __init__(): Constructor, sets up board info using the passed screen dimensions
-    def __init__(self, screen):
-        screen_width: int = screen.get_width()
-        screen_height: int = screen.get_height()
+    screen_xy: Tuple[int, int] = (0, 0)
+    margin_xy: Tuple[float, float] = (0, 0)
 
-        self.board_size: float = min(screen_width, screen_height) * self.SCALE_MODIFIER
-        self.cell_size: float = self.board_size / 6
-        self.margin_x: float = (screen_width - self.board_size) / 2
-        self.margin_y: float = (screen_height - self.board_size) / 2
-
+    # __init__(): Constructor, sets up board info using the passed screen
+    def __init__(self, screen) -> None:
+        self.screen = screen # Temporary
+        self.updateScreenInfo(screen)
         self.squares: List[List[Square]] = []
         self.draw()
 
+    # updateScreenInfo(): Update the screen size and margins
+    def updateScreenInfo(self, screen) -> None:
+        screen_width = screen.get_width()
+        screen_height = screen.get_height()
+
+        self.board_size: float = min(screen_width, screen_height) * self.SCALE_MODIFIER
+        self.square_size: float = self.board_size / 6
+        margin_x: float = (screen_width - self.board_size) / 2
+        margin_y: float = (screen_height - self.board_size) / 2
+
+        Board.screen_xy = (screen_width, screen_height)
+        Board.margin_xy = (margin_x, margin_y)
+
     # draw(): Determines where to place squares, and then creates them.
-    def draw(self):
-        for row in range(self.DIMENSIONS):
+    def draw(self) -> None:
+        for row in range(DIMENSIONS):
             square_row: List[Square] = []
-            for col in range(self.DIMENSIONS):
+            for col in range(DIMENSIONS):
                 is_even = (row + col) % 2 == 0
                 color = self.WHITE if is_even else self.BLACK
-                new_square: Square = Square(row, col, color)
+                new_square: Square = Square(self.square_size, row, col, color)
+                self.initializePiece(new_square, row, col)
                 square_row.append(new_square)
-                # We probably want to initialize each Square with its proper Piece here.
-                # e.g rename this function to "init_squares"
-                # Maybe we pass squares an "anchor coordinate"? They can use row & col to calculate where to place themselves based on that
-
+        
             self.squares.append(square_row)
 
         pygame.display.flip()
 
+    def initializePiece(self, square: 'Square', row: int, col: int):
+        if 2 <= row <= 3:
+            return # No pieces in the middle two rows
+        
+        color = 'b' if row < 2 else 'w'
+        piece_type = None
+
+        if row == 1 or row == 4:
+            piece_type = "pawn"
+
+        square.piece = Piece(color, piece_type, self.screen, square)
+
 class Square:
     # __init__(): Constructor
-    def __init__(self, row: int, col: int, color):
+    def __init__(self, size, row: int, col: int, color) -> None:
         self.row: int = row
         self.col: int = col
+        self.size = size
         self.color: Tuple[int, int, int] = color
         self.piece: Optional[Piece] = None
         self.is_selected: bool = False
-        # Call the square's draw function here
+        
+        self.draw(color)
         
     # draw(): Draws a single square to the screen at the specified coordinates
-    def draw(self, screen, x: int, y: int, size: float):
-        pygame.draw.rect(screen, self.color, (x, y, size, size))
-        # Check if this square has a piece. If so, call that piece's draw function
+    def draw(self, color) -> None:
+        pos_xy = self.getPosition()
+
+        pygame.draw.rect(pygame.display.get_surface(), color, (pos_xy[0], pos_xy[1], self.size, self.size))
+        if self.piece is not None:
+            self.piece.draw()
+
+    def getPosition(self) -> Tuple[float, float]:
+        margin_xy = Board.margin_xy
+        x = self.size * self.col + margin_xy[0]
+        y = self.size * self.row + margin_xy[1]
+        return (x, y)
 
 class Piece:
-    def __init__(self, row, col, color):
-        self.row = row
-        self.col = col
-        self.color = color
-        self.type = None
-        self.sprite = None
+    SCALE_MODIFIER = 0.8
 
-    def draw(self, screen):
+    def __init__(self, color, type, screen, square) -> None:
+        self.parent: 'Square' = square
+        self.color: Tuple[int, int, int] = color
+        self.type = type
+        self.sprite = None
+        self.loadSprite()
+        self.draw(screen)
+
+    def draw(self, screen) -> None:
+        if self.type is None or self.sprite is None:
+            return
+    
+        margin_xy = Board.margin_xy
+        square_pos = self.parent.getPosition()
+        square_size = self.parent.size
+
+        piece_x = square_pos[0] + (square_size - self.sprite.get_width()) / 2
+        piece_y = square_pos[1] + (square_size - self.sprite.get_height()) / 2
+        screen.blit(self.sprite, (piece_x, piece_y))
+
+    def loadSprite(self) -> None:
+        if self.type is None:
+            return
+
         try:
-            path = f'assets/{self.color}_king.png' # Temporary
-            self.sprite = pygame.image.load(path).convert_alpha()
-            screen.blit(self.sprite,(0,0))
+            path = f'assets/{self.color}_{self.type}.png' # Temporary
+            new_sprite = pygame.image.load(path).convert_alpha()
+
+            scale_size = int(self.parent.size * self.SCALE_MODIFIER)
+            self.sprite = pygame.transform.scale(new_sprite, (scale_size, scale_size))
+
         except pygame.error as err:
-            print(f"Error loading image. {err}")
+            print(f"Error loading image for {self.color}, {self.type}: {err}")
