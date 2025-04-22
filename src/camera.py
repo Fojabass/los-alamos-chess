9acf3d65afe13b1d7460205d9f0c8c9492f5ce05
Julien Devol <julien.devol@gmail.com>
2025-04-22 18:28:52 -0400

    Updated camera to use static methods

--- src/camera.py
+++ src/camera.py
@@ -5,31 +5,35 @@ import pygame
 
 class Camera:
     SPEED: float = 200
+    
     position = [0, 0]
-    screen_width = 0
-    screen_height = 0
+    screen = None
 
     def __init__(self, screen) -> None:
-        Camera.updateScreenInfo(screen)
-        Camera.position[0] = Camera.screen_width / 2
-        Camera.position[1] = Camera.screen_height / 2
+        Camera.addScreenRef(screen)
+        Camera.position[0] = Camera.getWidth() / 2
+        Camera.position[1] = Camera.getHeight() / 2
 
     # move(): Moves the camera in the passed directions.
     def move(self, dir_x: int = 0, dir_y: int = 0, delta_time: float = 0) -> None:
         Camera.position[0] += (dir_x * Camera.SPEED * delta_time)
         Camera.position[1] += (dir_y * Camera.SPEED * delta_time)
 
-    def updateScreenInfo(screen) -> None:
-        Camera.screen_width = screen.get_width()
-        Camera.screen_height = screen.get_height()
+    @staticmethod
+    def addScreenRef(screen) -> None:
+        Camera.screen = screen
+
+    @staticmethod
+    def getScreen():
+        return Camera.screen
 
     @staticmethod
     def getWidth():
-        return Camera.screen_width
+        return Camera.screen.get_width()
     
     @staticmethod
     def getHeight():
-        return Camera.screen_height
+        return Camera.screen.get_height()
     
     @staticmethod
     def getX():
